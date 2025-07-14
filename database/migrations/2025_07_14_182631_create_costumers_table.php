7d3880447ecb1ca21eab692c1b2915a855fc3fcb
Pedro Lucas <izielucas2016@gmail.com>
2025-07-14 15:31:39 -0300

    feat: added columns to costumers table

--- database/migrations/2025_07_14_182631_create_costumers_table.php
+++ database/migrations/2025_07_14_182631_create_costumers_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('costumers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type'); // individual or business
+            $table->string('email');
+            $table->string('city');
+            $table->string('state'); 
+            $table->string('postal_code');
             $table->timestamps();
         });
     }
