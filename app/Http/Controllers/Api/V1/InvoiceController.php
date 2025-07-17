962360d275e82b37873a066831d26b1a9cd93f81
Pedro Lucas <izielucas2016@gmail.com>
2025-07-17 10:23:58 -0300

    feat: added paginate get all invoices and get one invoice by id

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -6,6 +6,8 @@ use App\Models\Invoice;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
 use App\Models\Customer;
 
 class InvoiceController extends Controller
@@ -15,7 +17,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        
+        return new InvoiceCollection(Invoice::paginate());
     }
 
     /**
@@ -39,7 +41,7 @@ class InvoiceController extends Controller
      */
     public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
